83daf32a9a429e35be9f54b22a6149893933970b
waynerferreira <waynerferreira@hotmail.com>
2025-04-08 23:03:35 -0300

    fix:first commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "service_name" {}
+variable "cluster_name" {}
+variable "vpc_id" {}
+variable "private_subnets" {}
+variable "service_port" {}
+variable "service_cpu" {}
+variable "service_memory" {}
+variable "service_listener" {}
+
+
